e8c839d037c375f398a35699d43aaa057720e238
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-27 01:41:53 +0530

    calculate the sum of all numbers from 1 to 100 using a while loop.

---
+++ sumOfNumByWhileLoop.java
@@ -0,0 +1,17 @@
+class sumOfNumByWhileLoop {
+    public static void main(String[] args) {
+        //11. Write a program to calculate the sum of all numbers from 1 to 100 using a while loop.
+
+        int number1 = 1, number2=100;
+
+        int total=0;
+        while(number1<=number2){
+            total = total+number1;
+            number1++;
+        }
+        System.out.println("The total sum of 1 to 100 is: "+total);
+    }    
+}
+/**output:
+ * The total sum of 1 to 100 is: 5050
+ */
