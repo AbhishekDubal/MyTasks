88648d0b4735a54623844f46ae0f56143b4cd6bd
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-26 15:50:36 +0530

    Sum of 1 to 100 numbers

---
+++ sumOfNumbers.java
@@ -0,0 +1,12 @@
+//Write a program to calculate the sum of all numbers from 1 to 100.
+class sumOfNumbers {
+    public static void main(String[] args) {
+        int sum=0;
+
+        for (int i = 1; i <=100; i++) {
+            sum=sum+i;
+        }
+        System.out.print("The sum of Numbers 1 to 100 is: "+sum);
+    }    //output:-The sum of Numbers 1 to 100 is: 5050
+
+}
